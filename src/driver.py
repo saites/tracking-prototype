5a59de7aafbdd9b7acede03dd0247110e8e3c2b0
Alexander Saites <alexander@saites.dev>
2024-03-28 17:09:59 -0700

    fix some type-checker reports

--- src/driver.py
+++ src/driver.py
@@ -7,8 +7,8 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import Select
 
-from tracker import basemodel
 from tracker import datamodel as dm
+from tracker import datastore as ds
 from tracker import errors
 
 logging.basicConfig()
@@ -21,9 +21,8 @@ def main(command_file: typing.TextIO, output_file: typing.TextIO) -> None:
     This is called by default when this script is executed without arguments.
     In that case, `command_file` is `sys.stdin` and `output_file` is `sys.stdout`.
     """
-    engine = basemodel.get_sqlite_engine()
 
-    with Session(engine) as session:
+    with Session(ds.get_sqlite_engine()) as session:
         for line_num, line in enumerate(command_file):
             line = line.strip()
 
@@ -37,7 +36,7 @@ def main(command_file: typing.TextIO, output_file: typing.TextIO) -> None:
 
 
 # Create helper dicts to associate command strings to item types.
-_devices = {
+_devices: dict[str, type[dm.Device]] = {
     "SWITCH": dm.Switch,
     "DIMMER": dm.Dimmer,
     "LOCK": dm.Lock,
@@ -58,71 +57,70 @@ def process_command(session: Session, command: str, output_file: typing.TextIO) 
             return
 
         case ["NEW", "DWELLING", name]:
-            dm.new_dwelling(session, name)
+            ds.new_dwelling(session, name)
         case ["SET", "DWELLING", name, "TO", ("OCCUPIED" | "VACANT") as state]:
-            dm.set_dwelling_occupancy(session, name, dm.OccupancyState(state.lower()))
+            ds.set_dwelling_occupancy(session, name, dm.OccupancyState(state.lower()))
 
         case ["NEW", "HUB", name]:
-            dm.new_hub(session, name)
+            ds.new_hub(session, name)
         case ["INSTALL", hub_name, "INTO", dwelling_name]:
-            dm.install_hub(session, hub_name, dwelling_name)
+            ds.install_hub(session, hub_name, dwelling_name)
         case ["UNINSTALL", hub_name]:
-            dm.uninstall_hub(session, hub_name)
+            ds.uninstall_hub(session, hub_name)
 
         case ["PAIR", kind, device_name, "WITH", hub_name] if (cls := _devices.get(kind)):
-            dm.pair_device(session, cls, device_name, hub_name)
+            ds.pair_device(session, cls, device_name, hub_name)
         case ["UNPAIR", kind, device_name] if (cls := _devices.get(kind)):
-            dm.unpair_device(session, cls, device_name)
+            ds.unpair_device(session, cls, device_name)
 
         case ["NEW", "SWITCH", name]:
-            dm.new_switch(session, name)
+            ds.new_switch(session, name)
         case ["SET", "SWITCH", name, "TO", ("ON" | "OFF") as state]:
-            dm.set_switch_state(session, name, dm.SwitchState(state.lower()))
+            ds.set_switch_state(session, name, dm.SwitchState(state.lower()))
 
         case ["NEW", "DIMMER", name, "RANGE", low, "TO", high, "WITH", "FACTOR", factor]:
-            dm.new_dimmer(session, name, int(low), int(high), int(factor))
+            ds.new_dimmer(session, name, int(low), int(high), int(factor))
         case ["MODIFY", "DIMMER", name, "RANGE", low, "TO", high, "WITH", "FACTOR", factor]:
-            dm.update_dimmer(session, name, int(low), int(high), int(factor))
+            ds.update_dimmer(session, name, int(low), int(high), int(factor))
         case ["SET", "DIMMER", name, "TO", value]:
-            dm.set_dimmer_value(session, name, int(value))
+            ds.set_dimmer_value(session, name, int(value))
 
         case ["NEW", "LOCK", name, "WITH", "PIN", pin]:
-            dm.new_lock(session, name, pin)
+            ds.new_lock(session, name, pin)
         case ["MODIFY", "LOCK", name, "ADD", "PIN", pin]:
-            dm.add_lock_pin(session, name, pin)
+            ds.add_lock_pin(session, name, pin)
         case ["MODIFY", "LOCK", name, "REMOVE", "PIN", pin]:
-            dm.remove_lock_pin(session, name, pin)
+            ds.remove_lock_pin(session, name, pin)
         case ["SET", "LOCK", name, "TO", "LOCKED"]:
-            dm.lock_door(session, name)
+            ds.lock_door(session, name)
         case ["SET", "LOCK", name, "TO", "UNLOCKED", "USING", pin]:
-            dm.unlock_door(session, name, pin)
+            ds.unlock_door(session, name, pin)
 
         case ["NEW", "THERMOSTAT", name, "WITH", "DISPLAY", "IN", ("C" | "F") as display]:
-            dm.new_thermostat(session, name, dm.ThermoDisplay(display.lower()))
+            ds.new_thermostat(session, name, dm.ThermoDisplay(display.lower()))
         case ["MODIFY", "THERMOSTAT", name, "WITH", "DISPLAY", "IN", ("C" | "F") as display]:
-            dm.update_thermostat(session, name, dm.ThermoDisplay(display.lower()))
+            ds.update_thermostat(session, name, dm.ThermoDisplay(display.lower()))
         case ["SET", "THERMOSTAT", name, "TO", ("OFF" | "HEAT" | "COOL" | "HEATCOOL") as mode]:
-            dm.set_thermo_mode(session, name, dm.ThermoMode(mode.lower()))
+            ds.set_thermo_mode(session, name, dm.ThermoMode(mode.lower()))
         case ["SET", "THERMOSTAT", name, "TARGET", "TO", low, "TO", high]:
-            mn, mx = int(low), int(high)
-            dm.set_thermo_set_points(session, name, mn, mx)
+            ds.set_thermo_set_points(session, name, int(low), int(high))
         case ["SET", "THERMOSTAT", name, "CURRENT", "TO", current]:
-            dm.set_thermo_current_temp(session, name, int(current))
+            ds.set_thermo_current_temp(session, name, int(current))
 
         case ["RENAME", kind, old_name, "TO", new_name] if (cls := _kinds.get(kind)):
-            dm.rename(session, cls, old_name, new_name)
+            ds.rename(session, cls, old_name, new_name)
         case ["DELETE", kind, name] if (cls := _kinds.get(kind)):
-            dm.delete(session, cls, name)
+            ds.delete(session, cls, name)
 
         case ["SHOW", kind, name] if (cls := _all_kinds.get(kind)):
             output_file.write(f"--{kind} '{name}'--\n")
-            show(select(cls).where(cls.name == name), session, output_file, depth=1)  # type: ignore
+            show(select(cls).where(cls.name == name), session, output_file, depth=1)
         case ["DETAIL", kind, name] if (cls := _all_kinds.get(kind)):
             output_file.write(f"--{kind} '{name}'--\n")
-            show(select(cls).where(cls.name == name), session, output_file)  # type: ignore
+            show(select(cls).where(cls.name == name), session, output_file)
         case [("DETAIL" | "LIST") as specificity, kind] if (cls := _plural_kinds.get(kind)):
             output_file.write(f"--All {kind}--\n")
-            q = select(cls.name) if specificity == "LIST" else select(cls)  # type: ignore
+            q = select(cls.name) if specificity == "LIST" else select(cls)
             show(q, session, output_file)
 
         case _:
--- src/tracker/basemodel.py
+++ src/tracker/basemodel.py
@@ -1,18 +1,7 @@
 import re
-import uuid
 from typing import Annotated, Iterable, Self
 
-from sqlalchemy import (
-    Engine,
-    Identity,
-    Integer,
-    MetaData,
-    StaticPool,
-    Text,
-    create_engine,
-    engine,
-    event,
-)
+from sqlalchemy import Identity, Integer, MetaData, Text
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
@@ -54,40 +43,6 @@ class AutoID(MappedAsDataclass):
     """Mixin to add a primary key column named 'id'."""
 
     id: Mapped[DbID] = mapped_column(Identity(always=True), primary_key=True, init=False)
-
-
-def get_sqlite_engine(db_name: str | None = None) -> Engine:
-    """Return an in-memory SQLite database engine, configured with sensible defaults."""
-
-    # Use an in-memory database, potentially shared among multiple threads.
-    # This requires a recent version of SQLite, but this project targets Python 3.11 anyway.
-    db_url = engine.URL.create(
-        drivername="sqlite",
-        database=db_name or f"file:{uuid.uuid4().int:x}",
-        query={"mode": "memory", "check_same_thread": "false", "uri": "true"},
-    )
-
-    db_engine = create_engine(db_url, poolclass=StaticPool)
-
-    # Fix transactional support in the default sqlite driver:
-    # - disable BEGIN on connect; emit it at the proper point
-    # - disable COMMIT before DDL
-    # - enable foreign key support
-    # See: https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#pysqlite-serializable
-    @event.listens_for(Engine, "connect")
-    def on_connect(dbapi_connection, connection_record):  # type: ignore
-        dbapi_connection.isolation_level = None
-        cursor = dbapi_connection.cursor()
-        cursor.execute("PRAGMA foreign_keys=ON")
-        cursor.close()
-
-    @event.listens_for(Engine, "begin")
-    def on_begin(conn):  # type: ignore
-        conn.exec_driver_sql("BEGIN")
-
-    metadata.create_all(db_engine)
-
-    return db_engine
 
 
 def _split_camel(s: str) -> Iterable[str]:

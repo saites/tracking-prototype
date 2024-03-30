dde1ab3fbaeb7a9166d0d879c8f65a16bf2b41a8
Alexander Saites <alexander@saites.dev>
2024-03-29 20:57:23 -0700

    set event configuration at specific engine level

--- src/tracker/datastore.py
+++ src/tracker/datastore.py
@@ -48,14 +48,14 @@ def get_sqlite_engine(db_name: str | None = None) -> Engine:
     # - disable COMMIT before DDL
     # - enable foreign key support
     # See: https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#pysqlite-serializable
-    @event.listens_for(Engine, "connect")
+    @event.listens_for(db_engine, "connect")
     def on_connect(dbapi_connection, connection_record):  # type: ignore
         dbapi_connection.isolation_level = None
         cursor = dbapi_connection.cursor()
         cursor.execute("PRAGMA foreign_keys=ON")
         cursor.close()
 
-    @event.listens_for(Engine, "begin")
+    @event.listens_for(db_engine, "begin")
     def on_begin(conn):  # type: ignore
         conn.exec_driver_sql("BEGIN")
 
@@ -121,7 +121,7 @@ class DataTransaction:
         return self.session.scalars(select(kind.name))
 
     def get_by_name(
-        self, kind: type[PlaceOrDeviceT], name: str, operation: str
+        self, kind: type[PlaceOrDeviceT], name: str, operation: str = "get"
     ) -> PlaceOrDeviceT:
         """Find an item by name."""
 
